4e64e1a5b5558231099d10a085f3a0c830b30536
Md. Mehedi Hassan Sharif <mehedi0014@gmail.com>
2022-10-17 12:22:26 +0600

    1st commit

---
+++ forEddPlugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: forEddPlugin
+ * Description: forEddPlugin is a WooCommerce plugin that allows you to create discount links for customers.
+ * Author: ThemeNcode
+ * Author URI: http://www.themencode.com
+ * Version: 1.0
+ * Text Domain: forEddPlugin
+ * Domain Path: /languages
+ *
+ * @package forEddPlugin
+ */
+
+
+ 
